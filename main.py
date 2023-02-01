e8555856319c2f2afa730cf82c78818821903a18
balazsmogyorodi <balazsmogyorodi@gmail.com>
2023-02-01 15:32:47 +0100

    eredmeny javitva

--- main.py
+++ main.py
@@ -4,9 +4,9 @@
 
 def eredmeny(jatekosPontok, gepPontok):
         if jatekosPontok > 21:
-                return "Játékos gyözött"
-        elif gepPontok > 21:
                 return "Játékos vesztett"
+        elif gepPontok > 21:
+                return "Játékos gyözött"
         else:
                 return "Döntetlen"
         """tesztesetek"""
