023fea936d673208dff4a244170f3c7f9f87cc89
balazsmogyorodi <balazsmogyorodi@gmail.com>
2023-02-01 15:23:17 +0100

    eredmény kiiró

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 """ Megoldás"""
 
 
+
 def eredmeny():
+        jatekos = 8
+        gep = 8
+        if jatekos > gep:
+                return "Játékos gyözött"
+        elif gep > jatekos:
+                return "Gép gyözött"
+        else:
+                return "Döntetlen"
         """tesztesetek"""
+
+
+
+
