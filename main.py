00f889da988a3dcef648941145aea97f8cb2a9c8
balazsmogyorodi <balazsmogyorodi@gmail.com>
2023-02-01 14:54:21 +0100

    ujra

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 """ A fő program"""
 
 
-"""A játékosok"""
-jatekos1 = [8, 5]
-jatekos2 = [2, 10]
-
-def osszeszamolas():
-
-        """ Itt fogja összeszámolni a két listát"""
+def eredmeny():
+        """tesztesetek"""
