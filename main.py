cd10b9dcc61dfd3e2c3cf4f55293a9ad7fc4a7c4
balazsmogyorodi <balazsmogyorodi@gmail.com>
2023-02-03 12:24:59 +0100

    teszteset

--- main.py
+++ main.py
@@ -4,25 +4,40 @@
 
 
 
-
 def pontszamitas(lapok: [int]):
         pontok: int = 0
         for i in range(len(lapok)):
                 pontok += lapok[i]
         return pontok
 
+
 def eredmeny(jatekosLapok: [int], gepLapok: [int]):
         jatekosPontok = pontszamitas(jatekosLapok)
         gepPontok = pontszamitas(gepLapok)
         if jatekosPontok > 21 or jatekosPontok < gepPontok:
-                return "Játékos vesztett"
+                szoveg = "Játékos vesztett"
         elif gepPontok > 21 or jatekosPontok > gepPontok:
-                return "Játékos gyözött"
+                szoveg = "Játékos gyözött"
         else:
-                return "Döntetlen"
+                szoveg = "Döntetlen"
+        return szoveg
 
 
 """tesztesetek"""
 
 
+def jatekosVezstettTeszt():
+        jatekosLista = [10, 3, 2, 7]
+        gepLista = [6, 4, 3]
+        kapott = eredmeny(jatekosLista, gepLista)
+        vart = "Játékos vesztett"
+        if kapott == vart:
+                print("A teszt sikeres")
+        else:
+                print("A teszt megbukott")
 
+def tesztek():
+        jatekosVezstettTeszt()
+
+
+tesztek()
